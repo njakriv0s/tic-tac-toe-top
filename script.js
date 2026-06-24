a355d8aeb3cd9f1d4ef147031b1133291bc9de79
njakriv0s <nick_j_akrivos@hotmail.com>
2026-06-24 11:45:53 +0300

    Create prototype game loop

--- script.js
+++ script.js
@@ -9,10 +9,18 @@ function createPlayer(name, symbol) {
     return {name, symbol};
 };
 
+// TEMPORARY
+
+for (let i = 0; i <9; i++) {
+    gameboard.getGameboard()[i] = i;
+}
+
+// TEMPORARY
+
 let playerX = createPlayer("Nick", "X");
 let playerO = createPlayer("Chris", "O");
 
-const playTurn = (function playsFirst() {
+const gameController = (function playsFirst() {
     const playerArray = new Array(2);
     let playerXPosition = Math.floor(Math.random() * 2);
     playerArray[playerXPosition] = playerX;
@@ -26,4 +34,24 @@ const playTurn = (function playsFirst() {
     return {getPlayerArray}
 })();
 
-console.log(playTurn.getPlayerArray());
+
+// DON'T FORGET TO COMMIT FROM HERE
+
+
+let gameLength = gameboard.getGameboard().length;
+
+let currentPlayer = gameController.getPlayerArray()[0];
+
+
+
+for (let i = 0; i < gameLength; i++) {
+    alert(gameboard.getGameboard());
+    let playerChoice = prompt(`Turn ${i + 1}. ${currentPlayer.name} choose you placement`);
+    gameboard.getGameboard()[playerChoice] = currentPlayer.symbol;
+
+    currentPlayer = currentPlayer === gameController.getPlayerArray()[0] ? gameController.getPlayerArray()[1] : gameController.getPlayerArray()[0];
+    
+}
+
+console.log(gameboard.getGameboard());
+
