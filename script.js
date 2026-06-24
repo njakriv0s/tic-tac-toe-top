ff2b9a01bc655064cc953bb0060f4a9aa6a35f75
njakriv0s <nick_j_akrivos@hotmail.com>
2026-06-24 10:19:59 +0300

    Add function to randomize starting player

--- script.js
+++ script.js
@@ -12,7 +12,18 @@ function createPlayer(name, symbol) {
 let playerX = createPlayer("Nick", "X");
 let playerO = createPlayer("Chris", "O");
 
-console.log(playerX.symbol);
-console.log(playerO.name);
+const playTurn = (function playsFirst() {
+    const playerArray = new Array(2);
+    let playerXPosition = Math.floor(Math.random() * 2);
+    playerArray[playerXPosition] = playerX;
+    if (playerXPosition === 0) {
+        playerArray[1] = playerO;
+    }
+    else {
+        playerArray[0] = playerO;
+    }
+    const getPlayerArray = () => playerArray;
+    return {getPlayerArray}
+})();
 
-console.log(gameboard.getGameboard())
+console.log(playTurn.getPlayerArray());
